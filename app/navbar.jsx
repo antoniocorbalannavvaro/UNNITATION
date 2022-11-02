3aea067e1151202d5aec4f272e8764456d1ba213
railaup <pablogarciarodriguez@MacBook-Pro-de-Pablo.local>
2022-11-02 17:19:52 +0100

    Parte del anotador

--- app/navbar.jsx
+++ app/navbar.jsx
@@ -6,13 +6,13 @@ export default function NavBar() {
     return (
         <div className="container-fluid gradient-background">
             <div className="row p-0 px-2">
-                <div className="col p-0 d-flex align-items-center justify-content-start ">
-                    <p className="logo">UNNITATION</p>
+                <div className="col-3 p-0 d-flex align-items-center justify-content-start ">
+                    <a className="text-decoration-none" href="/"><p className="logo">UNNITATION</p></a>
                 </div>
-                <div className="col d-flex align-items-center justify-content-center">
+                <div className="col-6 d-flex align-items-center justify-content-center">
                     <UniMenu></UniMenu>
                 </div>
-                <div className="col d-flex align-items-center justify-content-end px-3">
+                <div className="col-3 d-flex align-items-center justify-content-end px-3">
                     <a href="/login"><UniButton>LOG IN</UniButton></a>
                 </div>
             </div>
