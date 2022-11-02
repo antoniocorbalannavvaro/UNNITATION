9e66cc16d7727ccdca4bb8c21b0dd76a98bfc4f3
antonio <antoniocn1996@gmail.com>
2022-11-02 10:03:19 +0100

    Second Commit

--- unnitation/src/App.js
+++ unnitation/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Prueba from './components/pure/prueba';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Prueba></Prueba>
+
       </header>
     </div>
   );
