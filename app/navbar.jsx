496f451012e035fc15662e85bf457ae8bbeec725
railaup <pablogarciarodriguez@MacBook-Pro-de-Pablo.local>
2022-11-02 13:03:53 +0100

    NavBar

--- app/navbar.jsx
+++ app/navbar.jsx
@@ -4,12 +4,13 @@ import UniButton from "./(components)/UniButton";
 export default function NavBar() {
     return (
         <div className="container-fluid gradient-background">
-            <div className="row p-0">
-                <div className="col p-0">
+            <div className="row p-0 px-2">
+                <div className="col p-0 d-flex align-items-center justify-content-start ">
                     <p className="logo">UNNITATION</p>
                 </div>
-                <div className="col">
-                    <UniButton onClick={() => console.log('Hola')}>Holaaa</UniButton>
+                <div className="col"></div>
+                <div className="col d-flex align-items-center justify-content-end px-3">
+                    <a href="/login"><UniButton onClick={() => console.log('Hola')}>LOG IN</UniButton></a>
                 </div>
             </div>
             
