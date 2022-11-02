387ccc62a79ddcc316e766a03f78c47bcd522d3b
antonio <antoniocn1996@gmail.com>
2022-11-02 09:59:03 +0100

    First Commit

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Prueba from './components/pure/prueba';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Prueba></Prueba>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/pure/prueba.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Prueba = () => {
+    return (
+    <div>
+        <h1>Vamoooos</h1>
+    </div>
+    );
+}
+
+export default Prueba;
