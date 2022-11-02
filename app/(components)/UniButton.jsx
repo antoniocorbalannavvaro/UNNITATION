0abd58f1ea9f1b8aede30058e0f3bf767e81bc98
railaup <pablogarciarodriguez@MacBook-Pro-de-Pablo.local>
2022-11-02 12:48:48 +0100

    Fixed bug

--- app/(components)/UniButton.jsx
+++ app/(components)/UniButton.jsx
@@ -2,6 +2,6 @@ import styles from './UniButton.module.css'
 
 export default function UniButton(props) {
     return (
-        <button style={styles.button}>{props.children}</button>
+        <button className={styles.button}>{props.children}</button>
     );
 }
