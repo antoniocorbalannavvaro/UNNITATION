022cac769fdb66d88d7997aa46d96cccb4084210
railaup <pablogarciarodriguez@MacBook-Pro-de-Pablo.local>
2022-11-03 17:29:57 +0100

    Cambios en los enlaces del menu

--- app/(components)/UniMenu.jsx
+++ app/(components)/UniMenu.jsx
@@ -1,14 +1,15 @@
 import styles from './UniMenu.module.css'
+import Link from 'next/link'
 
 export default function UniMenu(){
     return(
         <ul className={styles.list}>
-            <a href="/dashboard/videos"><li>Videos</li></a>
-            <a href="/dashboard/datasets"><li>Datasets</li></a>
-            <a href="/dashboard/labels"><li>Labels</li></a>
-            <a href="/dashboard/experiments"><li>Experiments</li></a>
-            <a href="/dashboard/users"><li>Users</li></a>
-            <a href="/dashboard/annotation"><li>Annotation</li></a>
+            <Link href="/dashboard/videos"><li>Videos</li></Link>
+            <Link href="/dashboard/datasets"><li>Datasets</li></Link>
+            <Link href="/dashboard/labels"><li>Labels</li></Link>
+            <Link href="/dashboard/experiments"><li>Experiments</li></Link>
+            <Link href="/dashboard/users"><li>Users</li></Link>
+            <Link href="/dashboard/annotation"><li>Annotation</li></Link>
         </ul>
     )
 }
