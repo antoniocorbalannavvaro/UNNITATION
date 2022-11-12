fba1f599fe7d8ac6a78169fd357ab1a7855b0e3a
David <david@lenovo>
2022-11-12 18:32:02 +0100

    Backend updates
    PostgreSQL dependency was added
    Added API entry points:
    	get-meta-fields: Get all the available app metafields
    	video/list: List all the available videos
    Database module was added

---
+++ pages/api/video/list.js
@@ -0,0 +1,26 @@
+import database from '../../database';
+
+export default async (req, res) => {
+	const dbRes = await database.query('SELECT name, url, transcriptUrl, numActors, videoDate, uploadDate, platform, language FROM Video');
+	
+	const videos = [];
+	
+	/* TODO: Do it asynchronously (using cursors) */
+	for (const row of dbRes.rows)
+	{
+		videos.push({
+			name: row.name,
+			properties: [
+				{id: 'url', value: row.url},
+				{id: 'transcriptUrl', value: row.transcripturl},
+				{id: 'numActors', value: row.numactors},
+				{id: 'videoDate', value: row.videodate},
+				{id: 'uploadDate', value: row.uploaddate},
+				{id: 'platform', value: row.platform},
+				{id: 'language', value: row.language}
+			]
+		});
+	};
+	
+	res.send(videos);
+};
