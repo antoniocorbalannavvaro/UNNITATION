ef6727fa14495a2fe44663ad898b7ba730d897cd
David <david@lenovo>
2022-11-24 18:18:17 +0100

    Backend updates

--- pages/api/get-meta-fields.js
+++ pages/api/get-meta-fields.js
@@ -6,8 +6,8 @@ export default (req, res) => {
 		videoDate: { type: 'datetime', label: 'Video date' },
 		platform: { type: 'select', label: 'Platform', options: ['GOOGLE_MEET', 'ZOOM', 'MICROSOFT_TEAMS'] },
 		language: { type: 'select', label: 'Language', options: ['SPANISH', 'ENGLISH', 'INDIAN', 'CHINESE'] },
-		languageLevel: { type: 'select', label: 'Language level', options: ['A1', 'A2', 'B1', 'B2', 'C1', 'C2', 'NATIVE'] },
 		gender: { type: 'select', label: 'Gender', options: ['MALE', 'FEMALE', 'TRANS', 'NON_BINARY', 'NOT_APPLICABLE'] },
-		departament: { type: 'select',  label: 'Departament', options: ['SALES', 'ENGINEERING', 'SUPPORT', 'CALL_CENTER'] }
+		departament: { type: 'select',  label: 'Departament', options: ['SALES', 'ENGINEERING', 'SUPPORT', 'CALL_CENTER'] },
+		userRole: { type: 'select', label: 'Role', options: ['ADMINISTRATOR', 'DATA_SCIENTIST', 'ANNOTATOR'] }
 	});
 };
