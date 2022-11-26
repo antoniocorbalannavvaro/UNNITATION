fb7557bfd346bcfc236e28307f0bd353d066dbe3
David <david@lenovo>
2022-11-26 20:18:01 +0100

    Backend updates (dummy)

--- pages/api/video/list.js
+++ pages/api/video/list.js
@@ -1,26 +1,41 @@
-import database from '../../main/database';
+/* TDOO */
+
+const dummyVideos = [
+	{
+		name: 'IBM tech meeting',
+		url: 'https://amazon.com/s3/videos/1234567890',
+		transcriptUrl: 'https://amazon.com/s3/videos/0987654321',
+		salesMeeting: false,
+		actorsInvolved: false,
+		videoDate: new Date(),
+		uploadDate: new Date(),
+		platform: 'GOOGLE_MEET',
+		language: 'INDIAN'
+	},
+	{
+		name: 'Intel sales meeting',
+		url: 'https://amazon.com/s3/videos/1234567890',
+		transcriptUrl: 'https://amazon.com/s3/videos/0987654321',
+		salesMeeting: true,
+		actorsInvolved: true,
+		videoDate: new Date(),
+		uploadDate: new Date(),
+		platform: 'MICROSOFT_TEAMS',
+		language: 'CHINESE'
+	},
+	{
+		name: 'Google API improvement discussion',
+		url: 'https://amazon.com/s3/videos/1234567890',
+		transcriptUrl: 'https://amazon.com/s3/videos/0987654321',
+		salesMeeting: false,
+		actorsInvolved: false,
+		videoDate: new Date(),
+		uploadDate: new Date(),
+		platform: 'ZOOM',
+		language: 'ENGLISH'
+	}
+];
 
 export default async (req, res) => {
-	const dbRes = await database.query('SELECT name, url, transcriptUrl, numActors, videoDate, uploadDate, platform, language FROM Video');
-	
-	const videos = [];
-	
-	/* TODO: Do it asynchronously (using cursors) */
-	for (const row of dbRes.rows)
-	{
-		videos.push({
-			name: row.name,
-			properties: [
-				{id: 'url', value: row.url},
-				{id: 'transcriptUrl', value: row.transcripturl},
-				{id: 'numActors', value: row.numactors},
-				{id: 'videoDate', value: row.videodate},
-				{id: 'uploadDate', value: row.uploaddate},
-				{id: 'platform', value: row.platform},
-				{id: 'language', value: row.language}
-			]
-		});
-	};
-	
-	res.send(videos);
+	res.json(dummyVideos);
 };
