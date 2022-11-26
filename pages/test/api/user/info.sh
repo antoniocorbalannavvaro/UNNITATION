efdb8f4ea9ecaa8f072a613ec12bd31666084812
David <david@lenovo>
2022-11-26 16:34:31 +0100

    Backend updates

--- pages/test/api/user/info.sh
+++ pages/test/api/user/info.sh
@@ -1,4 +1,5 @@
 source ../env.sh
 
 sessionId=`curl -v "$API_URL/user/login?username=andrea@hotmail.com&password=123" 2>&1 | grep "$SESSION_ID_COOKIE_LABEL=" | cut -f 2- -d = | cut -f 1 -d ";"`
-echo "Session ID: $sessionId"
+
+curl -b "$SESSION_ID_COOKIE_LABEL=$sessionId" $API_URL/user/info 2> /dev/null | json_reformat
