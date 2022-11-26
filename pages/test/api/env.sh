9f16fd87a6543e58969bb3cc3668fa4a9b648e65
David <david@lenovo>
2022-11-26 15:44:17 +0100

    Backend updates

--- pages/test/api/env.sh
+++ pages/test/api/env.sh
@@ -3,5 +3,7 @@ export APP_HOST=localhost
 export APP_PORT=3000
 export API_ROUTE=api
 export API_URL=$PROTOCOL://$APP_HOST:$APP_PORT/$API_ROUTE
+export DATABASE=unnitation
+export SESSION_ID_COOKIE_LABEL="SESSION_ID"
 
 echo "API url: $API_URL"
--- pages/test/api/user/invite.sh
+++ pages/test/api/user/invite.sh
@@ -1,6 +1,6 @@
 source ../env.sh
 
 # TODO: check the output!!
-curl --json '{"email": "andrea@hotmail.com", "roles": [ "ANNOTATOR", "ADMINISTRATOR" ], "annotationDedicationTime": 20}' $API_URL/user/invite ; echo
-curl --json '{"email": "sandra@gmail.com", "roles": [ "DATA_SCIENTIST" ]}' $API_URL/user/invite ; echo
-curl --json '{"email": "pedro@yahoo.com", "roles": [ "ANNOTATOR" ]}' $API_URL/user/invite ; echo
+curl --json '{ "email": "andrea@hotmail.com", "roles": [ "ANNOTATOR", "ADMINISTRATOR" ], "annotationDedicationTime": 20 }' $API_URL/user/invite ; echo
+curl --json '{ "email": "sandra@gmail.com", "roles": [ "DATA_SCIENTIST" ] }' $API_URL/user/invite ; echo
+curl --json '{ "email": "pedro@yahoo.com", "roles": [ "ANNOTATOR" ] }' $API_URL/user/invite ; echo
